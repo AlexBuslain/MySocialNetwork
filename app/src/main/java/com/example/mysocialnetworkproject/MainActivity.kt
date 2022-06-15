4789c8851cdc3d23831956ffe994062d9497473a
16205 <16205@ecam.be>
2022-06-15 17:38:39 +0200

    Add AddPostActivity call on floating button click

--- app/src/main/java/com/example/mysocialnetworkproject/MainActivity.kt
+++ app/src/main/java/com/example/mysocialnetworkproject/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.mysocialnetworkproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val newPostButton = findViewById<FloatingActionButton>(R.id.newPost)
+        newPostButton.setOnClickListener {
+            val intent = Intent(this, AddPostActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
