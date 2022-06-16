17591c0b2646f646f5dfb8d0571e42b57de03c7b
16205 <16205@ecam.be>
2022-06-16 22:39:21 +0200

    Comment and remove warning causes

--- app/src/main/java/com/example/mysocialnetworkproject/LoginActivity.kt
+++ app/src/main/java/com/example/mysocialnetworkproject/LoginActivity.kt
@@ -14,15 +14,15 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
+        // Initialize UI bindings
         val loginButton: Button = findViewById(R.id.login_button)
         val registerButton: Button = findViewById(R.id.register_button)
         val emailInput: EditText = findViewById(R.id.email_input)
         val pwdInput: EditText = findViewById(R.id.pwd_input)
-
-        val bundle: Bundle? = intent.extras;
-        val newlyRegisteredEmail: String? = bundle?.getString("email").toString()
-
+        // Get intent extras
+        val bundle: Bundle? = intent.extras
+        val newlyRegisteredEmail = bundle?.getString("email").toString()
+        // Autofill email input if user just registered
         if(newlyRegisteredEmail != "null") {
             Log.d("LoginActivity", "Email is $newlyRegisteredEmail")
             emailInput.setText(newlyRegisteredEmail)
@@ -30,6 +30,7 @@ class LoginActivity : AppCompatActivity() {
 
         loginButton.setOnClickListener {
         when {
+            // Check if email and password are empty
             TextUtils.isEmpty(emailInput.text.toString().trim { it <= ' ' }) -> {
                 Toast.makeText(
                     this@LoginActivity,
@@ -46,25 +47,24 @@ class LoginActivity : AppCompatActivity() {
                 ).show()
             }
             else -> {
-
+                // Trim email and password to remove unwanted spaces
                 val email: String = emailInput.text.toString().trim { it <= ' ' }
                 val password: String = pwdInput.text.toString().trim { it <= ' ' }
-
+                // Sign in user in Firebase
                 FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
                     .addOnCompleteListener { task ->
-
                         if (task.isSuccessful) {
                             Toast.makeText(
                                 this@LoginActivity,
                                 "Login successful",
                                 Toast.LENGTH_SHORT
                             ).show()
-
-
+                            // Go to main activity
                             val intent =
                                 Intent(this@LoginActivity, MainActivity::class.java)
                             intent.flags =
                                 Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                            // Forward user uid to main activity [unused]
                             intent.putExtra(
                                 "uid",
                                 FirebaseAuth.getInstance().currentUser!!.uid
@@ -77,7 +77,7 @@ class LoginActivity : AppCompatActivity() {
                 }
             }
         }
-
+        // Register button to launch register activity
         registerButton.setOnClickListener {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
