538bd677b51c529b55dfb8fa14d172d715b2c2a6
16205 <16205@ecam.be>
2022-06-16 22:23:09 +0200

    Display author by getting post uid and querying user collection

--- app/src/main/java/com/example/mysocialnetworkproject/MainActivity.kt
+++ app/src/main/java/com/example/mysocialnetworkproject/MainActivity.kt
@@ -1,3 +1,5 @@
+@file:Suppress("NAME_SHADOWING")
+
 package com.example.mysocialnetworkproject
 
 import android.annotation.SuppressLint
@@ -7,18 +9,18 @@ import android.graphics.text.LineBreaker.JUSTIFICATION_MODE_INTER_WORD
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.annotation.RequiresApi
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     @RequiresApi(Build.VERSION_CODES.O)
-    @SuppressLint("WrongViewCast")
+    @SuppressLint("WrongViewCast", "SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -29,16 +31,11 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        val user = Firebase.auth.currentUser
-            user?.let {
-                val uid = user.uid
-            }
-
         val db = Firebase.firestore
-        // val postCollection = db.collection("posts")
 
         val postsList = findViewById<LinearLayout>(R.id.post_iterator)
         val postCollection = db.collection("posts")
+        val usersCollection = db.collection("users")
 
         postCollection.orderBy("date", Query.Direction.DESCENDING).get()
             .addOnSuccessListener { result ->
@@ -48,7 +45,16 @@ class MainActivity : AppCompatActivity() {
                     post.setPadding(0, 10, 0, 40)
 
                         val postAuthor = TextView(this)
-                        postAuthor.text = document.get("author").toString()
+                        val uid = document.getString("uid")
+                        usersCollection.whereEqualTo("uid", uid).get()
+                            .addOnSuccessListener { result ->
+                                for (document in result) {
+                                    postAuthor.text = document.getString("firstname") + " " + document.getString("lastname")
+                                }
+                            }
+                            .addOnFailureListener { exception ->
+                                Log.w("TAG", "Error getting user's name and lastname: ", exception)
+                            }
                         postAuthor.setTextColor(Color.BLACK)
 
                         val postContent = TextView(this)
@@ -65,7 +71,7 @@ class MainActivity : AppCompatActivity() {
                 }
             }
             .addOnFailureListener { exception ->
-                println("Error getting posts: $exception")
+                Log.w("TAG","Error getting posts: $exception")
             }
     }
 }
