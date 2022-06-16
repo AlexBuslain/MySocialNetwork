2c8733377d680ec66d8bf52c2434dacead2790b8
Alex <alexandre@buslain.com>
2022-06-16 18:56:30 +0200

    Add Image Logic

--- app/src/main/java/com/example/mysocialnetworkproject/AddPostActivity.kt
+++ app/src/main/java/com/example/mysocialnetworkproject/AddPostActivity.kt
@@ -1,12 +1,65 @@
-
 package com.example.mysocialnetworkproject
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.storage.FirebaseStorage
+import kotlinx.android.synthetic.main.activity_add_post.*
+import java.util.*
 
 class AddPostActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_post)
+
+        add_image_button.setOnClickListener {
+            Log.d("AddPostActivity", "Try to see Add Picture")
+
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type="image/*"
+            startActivityForResult(intent, 0)
+        }
     }
+
+    ///////////// Stockage de la photo dans Firebase Storage /////////////
+
+    var selectedPhotoUri: Uri? = null
+
+    private fun UploadImageToFirebase() {
+        if (selectedPhotoUri == null) return
+
+        val filename = UUID.randomUUID().toString() // Nom du fichier = random ID
+        val ref = FirebaseStorage.getInstance().getReference("/images/$filename") // Référence vers le storage
+
+        ref.putFile(selectedPhotoUri!!)
+            .addOnSuccessListener {
+                Log.d("AddPostActivity", "Successfully upload")
+            }
+    }
+
+
+
+
+
+
+
+    //////// Code supplémentaire pour ajouter l'image selectionnée dans le "add picture" ////////
+    //override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    //    super.onActivityResult(requestCode, resultCode, data)
+            // On vient récupérer le résultat de la selection de la photo plus haut.
+    //    if (requestCode == 0 && resultCode == Activity.RESULT_OK && data != null) {
+
+    //        val uri = data.data
+    //        val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, uri)
+    //        val showimage = BitmapDrawable(bitmap)
+    //        add_image_button.setBackgroundDrawable(showimage)
+    //    }
+    //}
 }
