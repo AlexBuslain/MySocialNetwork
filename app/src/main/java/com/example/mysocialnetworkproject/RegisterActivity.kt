0fb419dc52572524a0bc47043b49900c35e6218c
16205 <16205@ecam.be>
2022-06-16 22:45:09 +0200

    Comment and remove warning causes

--- app/src/main/java/com/example/mysocialnetworkproject/RegisterActivity.kt
+++ app/src/main/java/com/example/mysocialnetworkproject/RegisterActivity.kt
@@ -7,8 +7,6 @@ import android.text.TextUtils
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.ktx.firestore
@@ -18,16 +16,16 @@ class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
-
+        // Initialize UI bindings
         val signInButton: Button = findViewById(R.id.sign_up_button)
         val firstnameInput: EditText = findViewById(R.id.firstname_input)
         val lastnameInput: EditText = findViewById(R.id.lastname_input)
         val emailInput: EditText = findViewById(R.id.email_input)
         val pwdInput: EditText = findViewById(R.id.pwd_input)
 
-
         signInButton.setOnClickListener {
             when {
+                // Check if all fields are filled in
                 TextUtils.isEmpty(firstnameInput.text.toString().trim { it <= ' ' }) -> {
                     Toast.makeText(
                         this@RegisterActivity,
@@ -67,53 +65,55 @@ class RegisterActivity : AppCompatActivity() {
 
                     // Create instance and register a user with email & password
                     FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
-                        .addOnCompleteListener(
-                            OnCompleteListener<AuthResult> { task ->
+                        .addOnCompleteListener { task ->
+                            // If registration is successful
+                            if (task.isSuccessful){
 
-                                // If registration is successful
-                                if (task.isSuccessful){
+                                // Firebase registered user
+                                val firebaseUser: FirebaseUser = task.result!!.user!!
+                                // Instantiate Firestore
+                                val db = Firebase.firestore
+                                // Create a new user document
+                                val user = hashMapOf(
+                                    // "uid" can be seen as a primary key in the database
+                                    "uid" to firebaseUser.uid,
+                                    "firstname" to firstname,
+                                    "lastname" to lastname,
+                                )
+                                // Add the user to the database
+                                db.collection("users")
+                                    .add(user)
+                                    .addOnSuccessListener {}
+                                    .addOnFailureListener { e ->
+                                        Toast.makeText(
+                                            this@RegisterActivity,
+                                            "Error: $e",
+                                            Toast.LENGTH_SHORT
+                                        ).show()
+                                    }
 
-                                    // Firebase registered user
-                                    val firebaseUser: FirebaseUser = task.result!!.user!!
-                                    val db = Firebase.firestore
-                                    val user = hashMapOf(
-                                        "uid" to firebaseUser.uid,
-                                        "firstname" to firstname,
-                                        "lastname" to lastname,
-                                    )
-                                    db.collection("users")
-                                        .add(user)
-                                        .addOnSuccessListener {}
-                                        .addOnFailureListener { e ->
-                                            Toast.makeText(
-                                                this@RegisterActivity,
-                                                "Error: $e",
-                                                Toast.LENGTH_SHORT
-                                            ).show()
-                                        }
-
-                                    Toast.makeText(
-                                        this@RegisterActivity,
-                                        "Sign up successful, please login with your new account",
-                                        Toast.LENGTH_LONG
-                                    ).show()
-
-                                    val intent =
-                                        Intent(this@RegisterActivity, LoginActivity::class.java)
-                                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                                    intent.putExtra("email", email)
-                                    startActivity(intent)
-                                    finish()
-                                } else {
-                                    // If registering not successful, show err message
-                                    Toast.makeText(
-                                        this@RegisterActivity,
-                                        task.exception!!.message.toString(),
-                                        Toast.LENGTH_SHORT
-                                    ).show()
-                                }
+                                Toast.makeText(
+                                    this@RegisterActivity,
+                                    "Sign up successful, please login with your new account",
+                                    Toast.LENGTH_LONG
+                                ).show()
+                                // Go to login activity
+                                val intent =
+                                    Intent(this@RegisterActivity, LoginActivity::class.java)
+                                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                                // Forward email to login activity in order to autofill the email field
+                                intent.putExtra("email", email)
+                                startActivity(intent)
+                                finish()
+                            } else {
+                                // If registering not successful, show err message
+                                Toast.makeText(
+                                    this@RegisterActivity,
+                                    task.exception!!.message.toString(),
+                                    Toast.LENGTH_SHORT
+                                ).show()
                             }
-                        )
+                        }
                 }
             }
         }
