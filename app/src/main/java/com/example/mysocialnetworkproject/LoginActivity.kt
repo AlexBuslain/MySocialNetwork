fe7878dcaef32a29966ce686f4e93053bcdfce83
16205 <16205@ecam.be>
2022-06-17 14:56:25 +0200

    Handle wrong password/email

--- app/src/main/java/com/example/mysocialnetworkproject/LoginActivity.kt
+++ app/src/main/java/com/example/mysocialnetworkproject/LoginActivity.kt
@@ -1,13 +1,13 @@
 package com.example.mysocialnetworkproject
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
@@ -72,6 +72,12 @@ class LoginActivity : AppCompatActivity() {
                             intent.putExtra("email", email)
                             startActivity(intent)
                             finish()
+                        } else {
+                            Toast.makeText(
+                                this@LoginActivity,
+                                "Invalid email or password",
+                                Toast.LENGTH_SHORT
+                            ).show()
                         }
                     }
                 }
